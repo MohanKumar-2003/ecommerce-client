68613424b6e285c2d82c94945fd4bc59baaea4de
MohanKumar-2003 <siva.mohan.kumar.2003@gmail.com>
2023-03-05 15:21:40 +0530

    first commit

--- src/Components/mainpages/auth/Register.js
+++ src/Components/mainpages/auth/Register.js
@@ -1,7 +1,7 @@
 import React,{useState} from "react";
 import {Link} from 'react-router-dom'
 import axios from 'axios'
-import mongoose from "mongoose";
+
 
 function Register(){
     const [user,setUser]=useState({
